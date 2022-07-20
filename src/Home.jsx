2125f47e5e1dc186882fd433d5128561dd0056eb
Bruno Silva <brunosco2002@gmail.com>
2022-07-20 12:40:45 -0300

    Add pages

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+
+  return (
+    <div>
+        Home Page
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </div>
+  )
+}
+
+export default Home
